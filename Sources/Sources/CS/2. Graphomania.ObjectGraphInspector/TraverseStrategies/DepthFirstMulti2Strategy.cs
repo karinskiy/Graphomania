fb70ecc08b2746d058770727f28bc3db60a6627d
karinskiy <karinskiy@atr-sz.ru>
2015-01-23 19:30:23 +0300

    DepthFirstMultiThreadStrategy

---
+++ Sources/Sources/CS/2. Graphomania.ObjectGraphInspector/TraverseStrategies/DepthFirstMulti2Strategy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphomania.ObjectGraphInspector.TraverseStrategies
+{
+    using System.Collections.Concurrent;
+
+    public class DepthFirstMulti2Strategy : ObjectGraphInspectorStrategy
+    {
+        public DepthFirstMulti2Strategy(ITraversedObjectRegistry traversedObjectRegistry, IObjectGraphVisitor graphVisitor, IReferenceProvider referenceProvider)
+            : base(traversedObjectRegistry, graphVisitor, referenceProvider)
+        {
+        }
+
+        public override void Inspect(object graphRoot)
+        {
+        }
+
+        private bool AlreadyTraversed(object graphRoot)
+        {
+            return traversedObjectRegistry.AlreadyTraversed(graphRoot);
+        }
+
+        private void MarkAsTraversed(object graphRoot)
+        {
+            lock (this)
+            {
+                traversedObjectRegistry.MarkAsTraversed(graphRoot);
+            }
+        }
+    }
+
+    public static class AsyncExtension
+    {
+        public static async Task<IEnumerable<T>> TraverseAsync<T>(this IEnumerable<T> source, Func<T, Task<IEnumerable<T>>> childSelector)
+        {
+            var results = new ConcurrentBag<T>();
+
+            Func<T, Task> foo = null;
+
+            foo = async next =>
+            {
+                results.Add(next);
+                var children = await childSelector(next);
+                await Task.WhenAll(children.Select(child => foo(child)));
+            };
+
+            await Task.WhenAll(source.Select(child => foo(child)));
+            return results;
+        }
+    }
+}
--- Sources/Sources/CS/2. Graphomania.ObjectGraphInspector/TraverseStrategies/DepthFirstMultiThreadStrategy.cs
+++ Sources/Sources/CS/2. Graphomania.ObjectGraphInspector/TraverseStrategies/DepthFirstMultiThreadStrategy.cs
@@ -1,5 +1,7 @@
 ﻿namespace Graphomania.ObjectGraphInspector.TraverseStrategies
 {
+    using System;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading;
